a052be246ed6e5b18e94f02813d0b6ae92962a7c
ZeyadAlshaikh <alshaikh.zeyad@gmail.com>
2023-09-03 14:24:26 +0300

    some update

--- Tutorials/Tutorial_1/EmployeeTest.java
+++ Tutorials/Tutorial_1/EmployeeTest.java
@@ -4,10 +4,14 @@ public class EmployeeTest {
         Employee emp2 = new Employee("Khaled", "Ibraheam", 100000);
         
         System.out.println(emp1);
+        System.out.println(emp1.getYearlySalary());
+
         System.out.println(emp2);
+        System.out.println(emp2.getYearlySalary());
         
         emp1.setMonthlySalary(emp1.getMonthlySalary() * 1.10);
         emp2.setMonthlySalary(emp2.getMonthlySalary() * 1.10);
+
         
         System.out.println(emp1);
         System.out.println(emp2);
